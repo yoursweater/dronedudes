555d6f212607f9e7b01b0ebba721ad653baa842b
Dan Eversen <dan_eversen@mckinsey.com>
2025-04-03 15:08:16 -0400

    create color data

---
+++ add_colors.py
@@ -0,0 +1,29 @@
+#Python code
+from codrone_edu.drone import *
+import time
+
+drone = Drone()
+drone.pair()
+
+
+dataset = "custom_color_data"
+colors = ["darkgreen", "white", "maroon", "pink", "yellow", "lightgreen", "blue", ]
+for color in colors:
+    data = []
+    samples = 500
+    for i in range(1):
+        print("Sample: ", i+1)
+        next = input("Press enter to calibrate " + color)
+        print("0% ", end="")
+        for j in range(samples):
+            color_data = drone.get_color_data()[0:9]
+            data.append(color_data)
+            time.sleep(0.005)
+            if j % 10 == 0:
+                print("-", end="")
+        print(" 100%")
+    drone.new_color_data(color, data, dataset)
+print("Done calibrating.")
+
+
+drone.close()
--- async_drone.py
+++ async_drone.py
@@ -3,26 +3,47 @@ import time
 from codrone_edu.drone import *
 
 async def monitor_position(drone):
-    for i in range(20):
-        # print(drone.get_pos_x())
-        distance = drone.get_front_range('cm')
-        print(distance)
-        await asyncio.sleep(0.2)
+    try:
+      for i in range(40):
+          # print(drone.get_pos_x())
+          distance = drone.get_front_range('cm')
+          print(f'distance: {distance}')
+
+          drone.load_color_data()
+          print('color data loaded?')
+          color_data = drone.get_color_data()
+          print('sensor data acquired?')
+          color = drone.predict_colors(color_data)
+          print(color_data)
+          print(f'predicted color: {color}')
+          if distance < 150 and i > 20:
+              drone.set_drone_LED(255, 0, 0, 100)
+              # drone.start_drone_buzzer(500)
+          await asyncio.sleep(0.2)
+    except Exception as e:
+        print('monitoring failed')
+        print(e)
+        drone.land()
+        drone.close()
 
 async def main():
-    drone = None
     try:
         # Initialize drone
         drone = Drone()
         drone.pair()
+        loop = asyncio.get_running_loop()
+        drone.set_drone_LED(0, 0, 255, 100)
+
+        
+        # drone.stop_drone_buzzer()
         
         # Start the monitoring task
         task = asyncio.create_task(monitor_position(drone))
         
         # Perform drone movements
-        loop = asyncio.get_running_loop()
         await loop.run_in_executor(None, drone.takeoff)
-        await loop.run_in_executor(None, lambda: drone.keep_distance(10, 30))
+        await loop.run_in_executor(None, lambda: drone.move_distance(0, 0, 0.25, 1))
+        await loop.run_in_executor(None, lambda: drone.keep_distance(8, 10))
         
         # Allow monitoring to complete (or cancel after movements)
         try:
@@ -32,6 +53,8 @@ async def main():
             
         # Land and close connection
         if drone:
+            drone.set_drone_LED(0, 0, 255, 100)
+            # drone.stop_drone_buzzer()
             drone.land()
             drone.close()
             
